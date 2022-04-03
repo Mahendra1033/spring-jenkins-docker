061c366ff22e115336e56f77e5d186426852bd3a
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-04-03 16:01:36 +0530

    first Commit

---
+++ src/main/java/com/springjenkinsdocker/SpringJenkinsDockerApplication.java
@@ -0,0 +1,21 @@
+package com.springjenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsDockerApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return " Docker With Jenkins :) ";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDockerApplication.class, args);
+	}
+
+}
